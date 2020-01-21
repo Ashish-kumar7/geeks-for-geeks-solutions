3d7346c5cb2d1932dd94558fcc4e0ff10e2470db
ashish007kumar <ashishkumar357ak@gmail.com>
2020-01-21 12:18:55 +0530

    flattening

---
+++ LinkedList/Flattening  Linked List.cpp
@@ -0,0 +1,25 @@
+Node * merge(Node * a,Node * b){
+    if(a==NULL){
+        return b;
+    }
+    if(b==NULL){
+        return a;
+    }
+    Node * result;
+    if(a->data < b->data){
+        result=a;
+        result->bottom=merge(a->bottom,b);
+    }
+    else {
+        result=b;
+        result->bottom=merge(a,b->bottom);
+    }
+    return result;
+}
+
+Node *flatten(Node *root){
+   if(root==NULL || root->next==NULL){
+       return root;
+   }
+   return merge(root,flatten(root->next));
+}
