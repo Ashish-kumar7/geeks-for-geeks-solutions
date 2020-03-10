92c9b729e4dd5b9eda73c2ca8ffa2ed5cf005cbf
ashish007kumar <ashishkumar357ak@gmail.com>
2020-03-10 17:02:41 +0530

    roman

---
+++ All Other Topics/Roman to decimal.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+int value(char r){ 
+	if (r == 'I') 
+		return 1; 
+	if (r == 'V') 
+		return 5; 
+	if (r == 'X') 
+		return 10; 
+	if (r == 'L') 
+		return 50; 
+	if (r == 'C') 
+		return 100; 
+	if (r == 'D') 
+		return 500; 
+	if (r == 'M') 
+		return 1000; 
+	return -1; 
+} 
+int romanToDecimal(string &str) {
+	int res = 0; 
+	for (int i=0; i<str.length(); i++){ 
+		// Getting value of symbol s[i] 
+		int s1 = value(str[i]); 
+		if (i+1 < str.length()) { 
+			int s2 = value(str[i+1]); 
+			if (s1 >= s2) { 
+				res = res + s1; 
+			} 
+			else{ 
+				res = res + s2 - s1; 
+				i++; 
+			} 
+		} 
+		else{ 
+			res = res + s1;
+		} 
+	} 
+	return res; 
+} 
+int main() { 
+	int t;
+	cin>>t;
+	while(t--){
+	   string str ;
+	cin>>str;
+	cout <<romanToDecimal(str) << endl;  
+	}
+	return 0; 
+} 
