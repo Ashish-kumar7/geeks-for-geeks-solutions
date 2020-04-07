af2da98c585e44fa790e4bc62291666ec4774a58
ashish007kumar <ashishkumar357ak@gmail.com>
2020-04-07 19:08:04 +0530

    graph

---
+++ Graph/Sum Of Depenedencies in Graph.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	//code
+	int t;
+	cin>>t;
+	while(t--){
+	int n;
+	int e;
+	cin>>n;
+	cin>>e;
+	int y=2*e;
+	int A[y];
+	for(int i=0;i<y;i++){
+	    cin>>A[i];
+	}
+	cout<<e<<endl;
+	}
+	return 0;
+}
