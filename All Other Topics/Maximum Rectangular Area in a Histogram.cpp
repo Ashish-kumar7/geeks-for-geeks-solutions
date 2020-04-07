bf60ce2191ada8716c647285e4da9b0f3a19f862
ashish007kumar <ashishkumar357ak@gmail.com>
2020-04-07 18:43:12 +0530

    Rectangle

---
+++ All Other Topics/Maximum Rectangular Area in a Histogram.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+	    long long int n;
+	    cin>>n;
+	    long long int A[n];
+	    for(int i=0;i<n;i++){
+	        cin>>A[i];
+	    }
+	 stack<long long int>S;
+	 long long int area=0;
+	long long int max_area=0;
+	long long  int i;
+	 for( i=0;i<n;){
+	    if(S.empty() || A[S.top()]<=A[i]){
+	     S.push(i);
+	     i++;
+	    }
+	    else{
+	        int top=S.top();
+	        S.pop();
+	        if(S.empty()){
+	            area=A[top] * i;
+	        }
+	        else{
+	            area=A[top] * (i-S.top()-1);
+	        }
+	        if(max_area<area){
+	            max_area=area;
+	        }
+	    }
+	 }
+	 while(!S.empty()){
+	     long long int top=S.top();
+	        S.pop();
+	        if(S.empty()){
+	            area=A[top] * i;
+	        }
+	        else{
+	            area=A[top] * (i-S.top()-1);
+	        }
+	        if(max_area<area){
+	            max_area=area;
+	        }
+	 }
+	    cout<<max_area<<endl;
+	 }
+	return 0;
+}
