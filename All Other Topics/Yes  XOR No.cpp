bc70b2cf37722dc53eacbc49d5e0dfd9883e04e5
ashish007kumar <ashishkumar357ak@gmail.com>
2020-04-11 23:03:20 +0530

    2

---
+++ All Other Topics/Yes  XOR No.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    long long int  A[n];
+	    long long int B[n];
+	    for(int i=0;i<n;i++){
+	        cin>>A[i];
+	    }
+	    for(int j=0;j<n;j++){
+	        cin>>B[j];
+	    }
+        cout<<"Yes"<<endl;
+	    //xor always occur in apir for more detail check out the editorials  
+	}
+	return 0;
+}
