2c401bd40511b08e1a512933ee3ac3e5d5b683e4
ashish007kumar <ashishkumar357ak@gmail.com>
2020-05-06 21:38:06 +0530

    5

---
+++ DP/Count of strings that can be formed using a, b and c under given constraints.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+	//code
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    cout<<( (n*n*n) +(3*n)+ 2)/2<<endl;
+	}
+	return 0;
+}
