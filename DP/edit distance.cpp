0e7c1459a7842f2f0c154baa72b3027c16c3f7fe
ashish007kumar <ashishkumar357ak@gmail.com>
2020-10-01 00:12:38 +0530

    dp

--- DP/edit distance.cpp
+++ DP/edit distance.cpp
@@ -1,40 +1,48 @@
-#include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-int main()
- {
+
+int minDistance(string word1, string word2) {
+        
+        int a=word1.length();
+        int b=word2.length();
+        int dp[a+1][b+1];
+        dp[0][0]=0;
+        for(int i=1;i<=b;i++){
+            dp[0][i] = dp[0][i-1] + 1 ;
+        }
+        for(int i=1;i<=a;i++){
+            dp[i][0] = dp[i-1][0] + 1 ;
+        }
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=b;j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=min( dp[i-1][j-1] , min( dp[i-1][j] , dp[i][j-1] ) ) + 1 ;
+                }   
+            }   
+        }
+        return dp[a][b];
+    }
+
+int main(){
 	//code
 	int t;
 	cin>>t;
 	while(t--){
-	int m;
-	cin>>m;
-	int n;
-	cin>>n;
-	string str1;
-	string str2;
-	cin>>str1;
-	cin>>str2;
-	int dp[m+1][n+1];
+	int a,b;
+	cin>>a;
+	cin>>b;
 	
-	for(int i=0;i<=m;i++){
-	    for(int j=0;j<=n;j++){
-	        if(j==0){
-	            dp[i][j]=i;
-	        }
-	       else if(i==0){
-	            dp[i][j]=j;
-	        }
-	        
-	       else if(str2[j-1]==str1[i-1]){
-	            dp[i][j]=dp[i-1][j-1];
-	        }
-	        else if(str2[j-1]!=str1[i-1]){
-	            dp[i][j]=min(dp[i-1][j-1],min(dp[i][j-1],dp[i-1][j]))+1;
-	        }
-	    }
-	}
-	cout<<dp[m][n]<<endl;
+	string word1;
+	string word2;
+	cin>>word1;
+	cin>>word2;
+	
+	cout<<minDistance(word1,word2)<<endl;
+	
+	
 	}
 	return 0;
 }
