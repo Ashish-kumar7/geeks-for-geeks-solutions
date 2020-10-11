54ac722b5db4ca5fd73d5943fcf64bf9fbf017c0
ashish007kumar <ashishkumar357ak@gmail.com>
2020-10-11 09:33:33 +0530

    cpp

---
+++ All Other Topics/Game of cells.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	//code
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n%2==0){
+	        cout<<"Mike"<<endl;
+	    }
+	    else{
+	        cout<<"Harvey"<<endl;
+	    }
+	
+	}
+	return 0;
+}
