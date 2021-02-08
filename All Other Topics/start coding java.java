5064ad79741544bf777731b919d1402b0cafd96c
ashish007kumar <ashishkumar357ak@gmail.com>
2021-02-08 23:46:32 +0530

    cpp

---
+++ All Other Topics/start coding java.java
@@ -0,0 +1,11 @@
+class Geeks{
+    
+    // Function to print hello
+    static void printHello(){
+        
+        // Your code here
+        System.out.println("Hello World");
+        
+    }
+    
+}
