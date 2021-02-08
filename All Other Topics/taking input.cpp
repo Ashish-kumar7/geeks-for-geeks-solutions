1d1822475e01467378472abcff605f3b4e30c3f9
ashish007kumar <ashishkumar357ak@gmail.com>
2021-02-08 23:46:06 +0530

    cpp

---
+++ All Other Topics/taking input.cpp
@@ -0,0 +1,9 @@
+ void inputData()
+{
+       int a;
+       string b;
+      //Use cin to take input and cout a and b with a space between them. Also use an endl after output
+      cin>>a;
+      cin>>b;
+      cout<<a<<" "<<b<<endl;
+}
