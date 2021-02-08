c738a2df7125b285b6cd8b4ad4560ab35058d6fc
ashish007kumar <ashishkumar357ak@gmail.com>
2021-02-08 19:47:50 +0530

    cpp

---
+++ All Other Topics/Learning Macros.cpp
@@ -0,0 +1,8 @@
+
+void macros(int a,int b)
+{
+    //See the macro defined at the top of the code. Use that macro to find a*b
+    //Use only macro to complete this task
+    cout<<MULTI(a,b);
+    cout<<endl;
+}
