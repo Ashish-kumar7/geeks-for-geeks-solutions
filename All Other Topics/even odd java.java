403adcfbb2a15832aa9bfd08cd0396138f5e7228
ashish007kumar <ashishkumar357ak@gmail.com>
2021-02-08 23:48:35 +0530

    cpp

---
+++ All Other Topics/even odd java.java
@@ -0,0 +1,15 @@
+class Geeks {
+    // 
+	 static void evenOdd (int a,int b) 
+	 {
+		//Your code here
+		if(a%2==0){
+		    System.out.println(a);
+		    System.out.println(b);
+		}
+	     else{
+	         System.out.println(b);
+	         System.out.println(a);
+	     }
+	 }
+}
