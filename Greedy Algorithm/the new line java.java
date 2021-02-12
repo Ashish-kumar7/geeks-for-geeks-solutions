029383f8f73ee243948e33667003df53838399b3
ashish007kumar <ashishkumar357ak@gmail.com>
2021-02-12 14:28:42 +0530

    java

---
+++ Greedy Algorithm/the new line java.java
@@ -0,0 +1,13 @@
+class Geeks{
+    
+    // Function to print each word in single line
+    static void printIndividualLine(){
+        
+        // Your code here
+        System.out.println("Geeks");
+        System.out.println("for");
+        System.out.println("Geeks");
+        
+    }
+    
+}
