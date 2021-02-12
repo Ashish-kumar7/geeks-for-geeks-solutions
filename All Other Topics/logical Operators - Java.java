419f7a765edfdf3ed099df217bae52230e577cee
ashish007kumar <ashishkumar357ak@gmail.com>
2021-02-12 14:21:37 +0530

    cpp

---
+++ All Other Topics/logical Operators - Java.java
@@ -0,0 +1,8 @@
+
+class Geeks{
+    
+    static void logicOp(boolean a, boolean b){
+        /*output (a&&b), (a||b), and ((!a)&&(!b))separated by spaces*/
+        System.out.println( (a && b)  + " " + (a||b) + " " + ( (!a) && (!b) ) );
+    }
+}
