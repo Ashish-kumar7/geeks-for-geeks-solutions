15098135c990948470b64b6b48e404c0a786d7ce
ashish007kumar <ashishkumar357ak@gmail.com>
2021-02-12 14:22:38 +0530

    cpp

---
+++ All Other Topics/data types.cpp
@@ -0,0 +1,19 @@
+void dataTypes(int a, float b, double c, long long l,string d){
+    
+    // Some statements have been given to you
+    // to help you understand data types
+    // Please complete the same for correct result
+    // Or you can delete and write your own
+    float p =  b/c; //do b/c
+    
+    double q = b/a ; //do b/a
+    
+    int r = c/a  ; // do c/a
+    
+    long long m = r+l;//do r+l
+    
+    cout << p << " " << q << " " << r <<" "<< m <<endl;
+    
+    cout << d << " " << d[3] << endl;//write 3 for index
+
+}
