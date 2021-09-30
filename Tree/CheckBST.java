d233f618c8ce840e36e18e43e517111f40bf4006
bug_finder <viditkumar.vk@gmail.com>
2021-09-30 23:44:11 +0530

    Create CheckBST.java

---
+++ Tree/CheckBST.java
@@ -0,0 +1,106 @@
+// https://practice.geeksforgeeks.org/problems/check-for-bst/1
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        Node root = new Node(Integer.parseInt(ip[0]));
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        queue.add(root);
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            String currVal = ip[i];
+            if(!currVal.equals("N")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+                
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            if(!currVal.equals("N")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution g = new Solution();
+        	    if(g.isBST(root))
+			        System.out.println(1);
+			    else
+			        System.out.println(0);
+                t--;
+            
+        }
+    }
+}
+
+public class Solution
+{
+    boolean isBSTUtil(Node root, int min, int max){
+        if(root == null)
+            return true;
+        
+        if(root.data < min || root.data > max)
+            return false;
+        
+        return isBSTUtil(root.left, min, root.data-1) 
+                && isBSTUtil(root.right, root.data+1, max);
+    }
+
+    boolean isBST(Node root)
+    {
+        return isBSTUtil(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+}
