a943c94d697428269206df2a30f33e7a533f4fc7
Harsh Dubey <73060630+harshdubey11@users.noreply.github.com>
2021-10-14 20:23:41 +0530

    Update Middle element of linked list.cpp

--- LinkedList/Middle element of linked list.cpp
+++ LinkedList/Middle element of linked list.cpp
@@ -1,3 +1,6 @@
+//C++ code to find middle of a linked list
+// Use two pointers slow and fast , and move fast twice the steps of slow.
+// As soon as fast reaches the last node , the slow is always at the middle element.
 
  int getMiddle(Node* head) {
     Node* slow=head,*fast=head;
