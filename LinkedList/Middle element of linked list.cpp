a795d839697795d3fd84d5984bd630a7ab9b9c77
Harsh Dubey <73060630+harshdubey11@users.noreply.github.com>
2021-10-14 20:21:24 +0530

    Update Middle element of linked list.cpp

--- LinkedList/Middle element of linked list.cpp
+++ LinkedList/Middle element of linked list.cpp
@@ -1,34 +1,10 @@
-int count(Node * start,int &count1){
-    if(start==NULL){
-        return count1;
-    }
-    if(start!=NULL){
-        count1++;
-        count(start->next,count1);
-    }
-    return count1;
-}
 
-int count2(Node * start1,int & p,int & data1){
-    if(start1==NULL || p<=0){
-        return data1 ;
+ int getMiddle(Node* head) {
+    Node* slow=head,*fast=head;
+    while(fast!=NULL && fast->next!=NULL)
+        {
+        slow=slow->next;
+        fast=fast->next->next;
+        }
+        return slow->data;   
     }
-    if(start1!=NULL){
-        p--;
-        data1=start1->data;
-        count2(start1->next,p,data1);
-    }
-    return data1;
-}
-
-
-int getMiddle(Node *head){
-   Node * start=head;
-   int count1=0;
-   count(start,count1);
-   int x=count1;
-  int p=x/2 + 1;
-  int data1=0;
-   count2(head,p,data1);
-   return data1;
-}
